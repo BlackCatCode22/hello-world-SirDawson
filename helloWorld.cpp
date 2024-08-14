9960f65ed5fcad7dc1a0846504243df9ed790022
SirDawson <siraaramisdawson@gmail.com>
2024-08-14 11:53:42 -0700

    Add files via upload
    uploaded my first C++ program

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+sD 8/14/24
+Sir-Aaramis Dawson
+helloWorld.cpp
+This is not my first C++ program.
+It did take an hour to write though.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nHello World!\n\n" << "Another string.\n\n" << "Yet another string.\n\n";
+    return 0;
+}
